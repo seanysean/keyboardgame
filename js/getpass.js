410daf89442f7cfa93fe17eb2414d3f0cdd50e42
Sean <seanysean700@gmail.com>
2017-10-06 15:06:59 -0300

    More stuff

---
+++ js/getpass.js
@@ -0,0 +1,3 @@
+var submitEL = document.getElementById("submit");
+var accounts = JSON.parse(sessionStorage.getItem("userDB"));
+alert(accounts);
--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 var loginEL = document.getElementById("login");
 var usernameEL = document.getElementById("user");
 var passwordEL = document.getElementById("pass");
+var errorEL = document.getElementsByClassName("error")[0];
 var checkLogin = JSON.parse(sessionStorage.getItem("userDB"));
 var tempo;
 
@@ -12,11 +13,17 @@ loginEL.addEventListener("click", function() {
     if (JSON.stringify(checkLogin[i]) === JSON.stringify(tempo["input"])) {
       sessionStorage.setItem("loggedin",true);
       sessionStorage.setItem("username",checkLogin[i][0]);
+      sessionStorage.setItem("currentAccount",JSON.stringify(checkLogin[i]));
       window.location = "home.html";
       break;
     }
     else {
-      //console.log(checkLogin[i] + " " + tempo["input"])
+      console.log(errorEL);
+      setTimeout(function() {
+        errorEL.style.display = "block";
+        usernameEL.value = "";
+        passwordEL.value = "";
+      }, 500);
     }
   }
 });
